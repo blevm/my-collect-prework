5c97843e5e65bac303bd9d858761b46966b42c99
Michelle Blevins <mjblevins7@gmail.com>
2018-03-24 21:58:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  if block_given?
+    i = 0
 
+    while i < array.length
+      yield(array[i])
+        i = i + 1
+    end
+
+    array
+  else
+    "No block was given!"
+  end
+end
 
